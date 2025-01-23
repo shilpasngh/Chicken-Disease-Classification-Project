0bcc332e70c8ac01e32189a58388c1146d3eebbd
Shilpa Singh <shilpasingh@Chandradeeps-MacBook-Pro.local>
2025-01-23 18:52:25 -0500

    Folder structure added

--- template.py
+++ template.py
@@ -21,8 +21,20 @@ list_of_files=[
     "params.yaml",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"   
+    "research/trials.ipynb"
 ]
 
 for filepath in list_of_files:
     filepath=Path(filepath)
+    filedir,filename=os.path.split(filepath)
+
+    if filedir!="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory;{filedir} for the file : {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,"w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} is already exists")
